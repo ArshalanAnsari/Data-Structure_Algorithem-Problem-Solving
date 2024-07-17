61ae2faf6f8a00fc70e843c3c0eb03923ce5becf
Arshalan <ansariarsh048@gmail.com>
2024-07-17 11:28:01 +0530

    add commit

---
+++ 1.Arrays/4. SquareOfSortedArray/Solution.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+
+var sortedSquares = function(nums) {
+      // let n = nums.length
+      // let result = Array(n);
+      
+      // for(i=0; i<n; i++){                   (have some error)
+      //     result[i] = nums[i]**2;
+      // }
+      //     result[i] = result.sort((x,y) => x-y)
+      
+      let n = nums.length;
+      let result = Array(n);
+      let left = 0;
+      let right = n-1;
+      
+      for(i = n-1; i >= 0; i--){
+          if( nums[left]**2 < nums[right]**2 ){
+              result[i] = nums[right]**2
+              right--;
+          }else{
+              result[i] = nums[left]**2
+              left++;
+          }
+      }
+          return result
+      }
--- Readme.md
+++ Readme.md
@@ -1,6 +1,7 @@
 ### DSA Sheet
 
 ## Arrays And Two Pointers
-- [TwoSum](https://leetcode.com/problems/two-sum/)
-- [Reverse String](https://leetcode.com/problems/reverse-string/)
-- [TwoSum 2](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/1319397327/)
+- [TwoSum](https://leetcode.com/problems/two-sum)
+- [Reverse String](https://leetcode.com/problems/reverse-string)
+- [TwoSum 2](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/submissions/1319397327)
+- [SquareOfSortedArray](https://leetcode.com/problems/squares-of-a-sorted-array)
